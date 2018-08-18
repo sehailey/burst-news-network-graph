85eaf72a943ec4bdb2d223ebf21f05a69fd9dad0
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2018-08-18 15:20:52 -0400

    cleanup

--- client/app.js
+++ client/app.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
-import {Navbar} from './components'
-import Routes from './routes'
+import { Graph } from './components'
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
+    return (
+        <div>
+            <Graph />
+        </div>
+    )
 }
 
 export default App
--- client/index.js
+++ client/index.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom'
 import history from './history'
-import store from './store'
+
+import { Router } from 'react-router-dom'
+
 import App from './app'
 
-// establishes socket connection
-import './socket'
-
 ReactDOM.render(
-  <Provider store={store}>
     <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('app')
+        <App />
+    </Router>,
+    document.getElementById('app')
 )
